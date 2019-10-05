d4f80a361a369a0f27d20e22c51d3eb0ae50d7df
dokeeffe <derek0207@gmail.com>
2019-10-05 21:13:59 +0100

    initial import

--- allsky.py
+++ allsky.py
@@ -10,6 +10,7 @@ import zwoasi as asi
 import pickle
 import datetime
 import PIL
+from PIL import Image, ImageDraw, ImageFont 
 import numpy as np
 from logging.handlers import SysLogHandler
 import logging
@@ -54,12 +55,21 @@ class AllSkyCamera:
             if mean > self.lower_image_mean and mean < self.upper_image_mean:
                 logging.info('exposure complete, saving exposure time for next image')
                 pickle.dump( exptime, open( "/tmp/all_sky_exp_time.p", "wb" ) )
+                self.annotate_image(filename)
                 break
             elif len(auto_exp_measurements) < 2:
                 exptime = int(exptime * 1.2)
 
+    def annotate_image(self,filename):
+        image = Image.open(filename)
+        draw  = ImageDraw.Draw(image)
+        font  = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', 40)
+        draw.text((10,10),'All Sky Camera {}'.format(datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S %Z %Y UTC")),fill='#ffffff', font=font)
+        image.save(filename)
+
+
     def calculate_mean(self, image):
-        img = PIL.Image.open(image).convert("L")
+        img = Image.open(image).convert("L")
         imgarr = np.array(img)
         return np.mean(imgarr)
 
